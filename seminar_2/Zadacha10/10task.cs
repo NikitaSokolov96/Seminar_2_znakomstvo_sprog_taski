4d06a38a343f19ba3d59a81f04556e6a358953a1
NikitaSokolov96 <107963433+NikitaSokolov96@users.noreply.github.com>
2022-07-18 01:53:20 +0300

    Add files via upload

---
+++ seminar_2/Zadacha10/10task.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+void findSecondNumber(int start, int finish)
+{
+    int num = new Random().Next(start, finish);
+    Console.WriteLine(num);
+    int secondnum = num % 100 /10;
+    Console.WriteLine(secondnum);
+}
+
+findSecondNumber(100, 999);
---
+++ seminar_2/Zadacha13/13task.cs
@@ -0,0 +1,36 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+
+void findSecondNumber(int start, int finish)
+{
+    int num = new Random().Next(start, finish);
+    Console.WriteLine(num);
+    int secondnum = num % 10;
+    int thirdnum = num / 100 % 10;
+    int justnum = num % 100 / 10;
+    if (num > 9999)
+    {
+        Console.WriteLine(thirdnum);
+    }
+    else if (num > 999)
+    {
+        Console.WriteLine(justnum);
+    }
+    else if (num > 99)
+    {
+        Console.WriteLine(secondnum);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+findSecondNumber(10, 99999);
